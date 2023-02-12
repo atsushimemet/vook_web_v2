ab048f51ece7662caf1fb245f42334f47f4c50ab
Ubuntu <ubuntu@ip-172-31-38-168.ap-northeast-1.compute.internal>
2023-02-12 05:09:59 +0000

    prod: settings & poetry

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "52.197.151.34", "vook.tokyo"]
 
 
 # Application definition
