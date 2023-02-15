26ce76fcba408a25e1d7f2ff54d58a3e2ba288b1
atsushimemet <noap3b69n@gmail.com>
2023-02-15 22:50:55 +0900

    feat: #31 views.pyにproduct_pageを追加

--- vook_web/views.py
+++ vook_web/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "vook_web/index.html", {})
+
+
+def product_page(request, product_name):
+    # 商品ページのビュー関数
+    return render(request, "vook_web/product_page.html", {"product_name": product_name})
