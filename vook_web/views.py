3b8bcdc37cf994d3fb52fdb74c97b84d92b860ab
atsushimemet <noap3b69n@gmail.com>
2023-02-15 23:26:14 +0900

    close #31 (#32)
    * feat: #31 urls.pyにproduct_pageを追加
    
    * feat: #31 views.pyにproduct_pageを追加
    
    * feat: #31 productsコンテキストを追加
    
    * feat: #31 productsコンテキストの分割
    
    * feat: #31 index.htmlにテンプレート変数を反映
    
    * fix: #31 テンプレート変数の修正
    
    * fix: #31 products変数の修正
    
    * fix: #31 '/'を'-'に変更
    
    * feat: #31 全ての商品にリンクを追加
    
    * feat: #31 product_page.htmlの追加

--- vook_web/views.py
+++ vook_web/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "vook_web/index.html", {})
+    products = ["denim-501-50s-XX", "denim-501-50s-BigE", "denim-501-50s-赤耳"]
+    return render(
+        request,
+        "vook_web/index.html",
+        {
+            "product_0": products[0],
+            "product_1": products[1],
+            "product_2": products[2],
+        },
+    )
+
+
+def product_page(request, product_name):
+    # 商品ページのビュー関数
+    return render(request, "vook_web/product_page.html", {"product_name": product_name})
