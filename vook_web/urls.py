a0558ab04ba90dd469f2390b4a0b82c2f7168d13
atsushimemet <noap3b69n@gmail.com>
2023-02-19 12:28:51 +0900

    feat: #37 urls.pyにsearchを追加

--- vook_web/urls.py
+++ vook_web/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
+    path("search/", views.search, name="search"),
     path("<str:product_name>/", views.product_page, name="product_page"),
 ]
