c3c5409caf533d9ae4d7fa9a0e450df5b3505379
atsushimemet <noap3b69n@gmail.com>
2023-02-19 12:14:05 +0900

    feat: #35 aboutページのurlを追加

--- vook_web/urls.py
+++ vook_web/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
     path("<str:product_name>/", views.product_page, name="product_page"),
 ]
