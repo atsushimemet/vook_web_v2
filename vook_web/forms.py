00614713fb38831c0d5d107aebad4769e3b3a23a
atsushimemet <noap3b69n@gmail.com>
2023-02-24 14:03:07 +0900

    feat: #54 SearchFormにcleanメソッドを追加

--- vook_web/forms.py
+++ vook_web/forms.py
@@ -25,3 +25,16 @@ class SearchForm(forms.Form):
         label="年代",
         widget=Select(attrs={"style": "width: 400px"}),
     )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        brand = cleaned_data.get("brand")
+        item = cleaned_data.get("item")
+        model = cleaned_data.get("model")
+        age = cleaned_data.get("age")
+
+        # ここでバリデーション処理を行う
+        # バリデーションに失敗した場合は、ValidationErrorを発生させる
+        # ValidationErrorはフィールド単位で発生させることも可能
+
+        return cleaned_data
