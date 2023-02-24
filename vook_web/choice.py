4782b77ec07bddfebb826c9c3a0b3f44960e7627
atsushimemet <noap3b69n@gmail.com>
2023-02-24 13:38:41 +0900

    feat: #54 CHOICEのグローバル変数を格納するchoiceを追加

---
+++ vook_web/choice.py
@@ -0,0 +1,37 @@
+CHOICES_BRAND = (
+    ("initial", "選択してください"),
+    ("levis", "Levi's"),
+    ("ralphlauren", "Ralph Lauren"),
+    ("champion", "Champion"),
+)
+CHOICES_ITEM = (
+    ("initial", "選択してください"),
+    ("denim", "Denim"),
+    ("denimjacket", "Denim Jacket"),
+    ("shirts", "Shirts"),
+    ("sweatshirts", "Sweat Shirt"),
+    ("hoodie", "Hoodie"),
+)
+CHOICES_MODEL = (
+    ("initial", "選択してください"),
+    ("501-40s-XX:大戦モデル", "501-40s-大戦モデル"),
+    ("501-40s-XX:革パッチ前期", "501-50s-XX:革パッチ前期"),
+    ("501-50s-XX:革パッチ後期", "501-50s-XX:革パッチ後期"),
+    ("501-50s-XX:紙パッチギャラ有り", "501-50s-XX:紙パッチギャラ有り"),
+    ("501-50s-XX:紙パッチギャラ無し", "501-50s-XX:紙パッチギャラ無し"),
+    ("501-60s-XX:紙パッチギャラ無し", "501-60s-XX:紙パッチギャラ無し"),
+    ("501-60s-XX&BIGE", "501-60s-XX&BIGE"),
+    ("501-60s-BIGE:前期", "501-60s-BIGE:前期"),
+    ("501-60s-BIGE:後期", "501-60s-BIGE:後期"),
+    ("501-60s-66前期", "501-60s-66前期"),
+    ("501-70s-66後期", "501-70s-66後期"),
+    ("501-80s-赤耳", "501-80s-赤耳"),
+)
+CHOICES_AGE = (
+    ("initial", "選択してください"),
+    ("40s", "40s"),
+    ("50s", "50s"),
+    ("60s", "60s"),
+    ("70s", "70s"),
+    ("80s", "80s"),
+)
